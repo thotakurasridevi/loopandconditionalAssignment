39428019d3c4c34d6914cc654c308d723ac97d30
thotakurasridevi <102217403+thotakurasridevi@users.noreply.github.com>
2023-04-01 08:31:25 +0530

    Add files via upload

---
+++ substring.java
@@ -0,0 +1,11 @@
+import javax.print.event.PrintEvent;
+
+class Substring{  
+    public static void main(String args[]){  
+    String s1="Mangalyaan";
+    String s2="Dinosaur";  
+    System.out.println(s1.substring(0,5+1));
+    System.out.println(s2.substring(4,7+1));
+
+    }
+}
